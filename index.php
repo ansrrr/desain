c818336d85d731684f1632ef5bc39110ca054de4
ansrrr <achmd.tgsklh12@gmail.com>
2025-06-18 14:59:50 +0700

    commit

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
     include 'komponen/konten.php';
     include("komponen/footer.html");
+
 ?>
 
 
---
+++ komponen/data.php
@@ -0,0 +1,12 @@
+<?php
+    $prodi = [
+        'Teknologi Informasi' => 31,
+        'Akuntansi'           => 20,
+        'Budidaya Ikan'       => 6
+    ];
+
+    $gender = [
+        'Laki-laki' => 10,
+        'Perempuan' => 30
+    ];
+?>
